0a39d3ad86523a91ee4e326c5e4096956b553169
Louai Al-Khanji <louai@fb.com>
2021-03-02 13:24:35 +0000

    Vendor subgraph compile plugin

--- src/rebar3_bsp.erl
+++ src/rebar3_bsp.erl
@@ -3,5 +3,7 @@
 -export([init/1]).
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
-  rebar3_bsp_prv:init(State).
+init(State0) ->
+  {ok, State1} = erl_subgraph_compile_prv:init(State0),
+  {ok, State2} = rebar3_bsp_prv:init(State1),
+  {ok, State2}.
